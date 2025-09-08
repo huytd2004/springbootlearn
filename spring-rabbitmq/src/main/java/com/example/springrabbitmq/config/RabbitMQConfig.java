e7f6002f573fb225e11208a5a1c76a7c0600364c
Trần Doãn Huy <Huy.TD225859@sis.hust.edu.vn>
2025-09-08 21:27:35 +0700

    update

--- spring-rabbitmq/src/main/java/com/example/springrabbitmq/config/RabbitMQConfig.java
+++ spring-rabbitmq/src/main/java/com/example/springrabbitmq/config/RabbitMQConfig.java
@@ -1,18 +1,13 @@
 package com.example.springrabbitmq.config;
 
-import com.rabbitmq.client.ConnectionFactory;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Queue;
 
 @Configuration
 public class RabbitMQConfig {
